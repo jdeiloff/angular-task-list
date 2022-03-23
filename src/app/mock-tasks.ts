5f2e07b4903613ee0142be44040ae9d8646f9c79
jdeiloff <jdeiloff@gmail.com>
2022-03-23 14:48:09 -0300

    Finishing -add task- button

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,15 @@
+import {Task} from "./task"
+export const TASKS: Task[] = [
+    {
+    id: 1,
+    text: 'terminar primer modulo de Angular',
+    day: 'Agosto 5 a las 12:00',
+    reminder: true
+    },
+    {
+    id: 2,
+    text: 'Hacer las compras para la cena',
+    day: 'Agosto 5 a las 17:45',
+    reminder: false
+    }
+]
