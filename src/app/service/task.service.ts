05c95caffb6a45bad18169d35570f5c47e9ad55c
jdeiloff <jdeiloff@gmail.com>
2022-03-26 23:36:20 -0300

    A bunch of bugfixes

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHandler} from '@angular/common/http'
+import {HttpClient, HttpHeaders} from '@angular/common/http'
 import {TASKS} from '../mock-tasks'
 import {Task} from '../task' 
 import {Observable, of} from 'rxjs'
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
 @Injectable({
   providedIn: 'root'
 })
@@ -13,7 +18,18 @@ export class TaskService {
     private http:HttpClient
   ) { }
 
-  getTastks(): Observable<Task[]>{
+  getTasks(): Observable<Task[]>{
     return this.http.get<Task[]>(this.apiUrl)
+  }
+  deleteTask(task:Task): Observable<Task>{
+    const url = '${this.apiUrl}/${task,id}'
+    return this.http.delete<Task>(url)
+  }
+  updateTaskReminder(task: Task): Observable<Task>{
+    const url = '${this.apiUrl}/${task.id}'
+    return this.http.put<Task>(url, task, httpOptions)
+  }
+  addTask(task:Task): Observable<Task>{
+    return this.http.post<Task>(this.apiUrl, task, httpOptions)
   }
 }
--- src/app/task.ts
+++ src/app/task.ts
@@ -1,5 +1,5 @@
 export interface Task {
-    id: number; //Cuando creamos task podria no venir
+    id?: number; //Cuando creamos task podria no venir
     text: string;
     day: string;
     reminder: boolean;
